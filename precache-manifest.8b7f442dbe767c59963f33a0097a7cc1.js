41d6e2148e2d60e56b883ceed79b3446709df7f0
vitalcha <vkrokhov@gmail.com>
2019-12-10 12:21:02 +0200

    Updates

---
+++ precache-manifest.8b7f442dbe767c59963f33a0097a7cc1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b06c3d773c4771d161e3705e14f97b36",
+    "url": "/index.html"
+  },
+  {
+    "revision": "93136de786aaa1e96c75",
+    "url": "/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "d1a13e063852ad951805",
+    "url": "/static/css/main.15db6be4.chunk.css"
+  },
+  {
+    "revision": "93136de786aaa1e96c75",
+    "url": "/static/js/2.ebb67934.chunk.js"
+  },
+  {
+    "revision": "d1a13e063852ad951805",
+    "url": "/static/js/main.7b959ea4.chunk.js"
+  },
+  {
+    "revision": "aa84a4d9efb63389e8bf",
+    "url": "/static/js/runtime-main.f6c260c5.js"
+  }
+]);
